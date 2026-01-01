c46be9204737be5d37449b600f2bff6fb75ff7e4
Mark <markbaltenko@gmail.com>
2026-01-01 15:44:15 +0200

    Update configuration and API references to use environment variables
    - Added environment variable support in next.config.mjs for NEXT_PUBLIC_APP_URL.
    - Updated openapi.json to reference the new environment variable for server URL.
    - Modified OpenAPI.ts to use the environment variable for the BASE URL, ensuring flexibility in deployment environments.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    NEXT_PUBLIC_APP_URL:
+      process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
+  },
+};
 
 export default nextConfig
 
